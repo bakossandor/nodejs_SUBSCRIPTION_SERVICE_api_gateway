745eaa2e0d9a8b358d9e993b574e80dcea17c04e
Sándor Bakos <bakos.webd@gmail.com>
2019-09-11 22:58:20 +0200

    need additional fixes

--- router/router.js
+++ router/router.js
@@ -1,10 +1,11 @@
 const getConfigFiles = require('../lib/getPathConfig');
+const authMiddleWare = require('../lib/authMiddleware');
 
 async function route (app) {
   const configFiles = await getConfigFiles();
   configFiles.forEach((service) => {
     service.routes.forEach((route) => {
-      
+      app[route.method](route.path, authMiddleWare(route.scope), )
     })
   })
   app.get('/', (req, res, next) => {
